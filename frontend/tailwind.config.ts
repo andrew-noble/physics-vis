8e20c5f3abdcbd6e67417c17d8b9bddaf8fa365e
Andrew Noble <aknoble.andrew@gmail.com>
2025-06-08 12:38:36 -0700

    again

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
+import animate from "tailwindcss-animate";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
-  plugins: [typography],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, animate],
 } satisfies Config;
